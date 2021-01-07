28c0b677919dcac877dc989fff83300ce4238968
aivan-cva <76573806+aivan-cva@users.noreply.github.com>
2021-01-06 23:45:45 -0600

    Deleted website

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // Navigation color change
-const sections = document.querySelectorAll("section");
-const navItems = document.querySelectorAll("a");
+// const sections = document.querySelectorAll("section");
+// const navItems = document.querySelectorAll("a");
 
-const options = {
-  threshold: 0.5,
-};
-const observeSection = (entries) => {
-  entries.forEach((entry) => {
-    const index = entry.target.dataset.index;
-    const navData = entry.target.dataset.nav;
+// const options = {
+//   threshold: 0.5,
+// };
+// const observeSection = (entries) => {
+//   entries.forEach((entry) => {
+//     const index = entry.target.dataset.index;
+//     const navData = entry.target.dataset.nav;
 
-    if (entry.isIntersecting) {
-      navItems[index].classList.add(`${navData}-active`);
-    } else {
-      navItems[index].classList.remove(`${navData}-active`);
-    }
-  });
-};
+//     if (entry.isIntersecting) {
+//       navItems[index].classList.add(`${navData}-active`);
+//     } else {
+//       navItems[index].classList.remove(`${navData}-active`);
+//     }
+//   });
+// };
 
-const observer = new IntersectionObserver(observeSection, options);
+// const observer = new IntersectionObserver(observeSection, options);
 
-const setObservers = () => {
-  sections.forEach((section) => {
-    observer.observe(section);
-  });
-};
+// const setObservers = () => {
+//   sections.forEach((section) => {
+//     observer.observe(section);
+//   });
+// };
 
-setObservers();
+// setObservers();
